fa8912b52210ca444639c7d24ac6623a220ef53f
hotaru <yaunm@ya.ru>
2021-06-26 15:11:40 +0300

    added custom timestamps

--- utils/weatherapi.py
+++ utils/weatherapi.py
@@ -14,7 +14,7 @@ class WeatherAPI:
         )
         response = requests.get(request)
         location = response.json()
-        if response.status_code == 200:  # Is anything else possible?
+        if response.status_code == 200:  # Is anything else possible? answer: everything is posiible...
             if location == []:
                 return None
 
@@ -26,32 +26,66 @@ class WeatherAPI:
         else:
             return None
 
-    def get_weather(self, name: str, language: str):
+    def get_weather(self, name: str, language: str, timestamp: str = None):
         if geo := self.get_geo(name):
             lat = geo["lat"]
             lng = geo["lng"]
 
-            response = requests.get(
-                f"{self.weather_url}lat={lat}&lon={lng}&appid={self.weather_token}&lang={language}&units=metric"
-            )
-            if response.status_code == 200:
-                json_data = response.json()
-                dt = int(json_data["list"][2]["dt"])
+            if timestamp == None:
+                response = requests.get(
+                    f"{self.weather_url}lat={lat}&lon={lng}&appid={self.weather_token}&lang={language}&units=metric"
+                )
+                if response.status_code == 200:
+                    json_data = response.json()
+                    dt = int(json_data["list"][2]["dt"])
 
-                return {
-                    "country": json_data["city"]["country"],
-                    "city": json_data["city"]["name"],
-                    "time": datetime.utcfromtimestamp(dt).strftime("%H:%M"),
-                    "summary": json_data["list"][2]["weather"][0]["description"],
-                    "apparentTemperature": json_data["list"][2]["main"]["feels_like"],
-                    "temperature": json_data["list"][2]["main"]["temp"],
-                    "wind": json_data["list"][2]["wind"]["speed"],
-                    "humidity": json_data["list"][2]["main"]["humidity"] / 100,
-                    "icon": json_data["list"][2]["weather"][0]["icon"],
-                    "+2": json_data["list"][4]["main"]["temp"],
-                    "+4": json_data["list"][6]["main"]["temp"],
-                    "+6": json_data["list"][8]["main"]["temp"],
-                    "+8": json_data["list"][10]["main"]["temp"],
-                    "+10": json_data["list"][12]["main"]["temp"],
-                    "+12": json_data["list"][14]["main"]["temp"],
-                }
+                    return {
+                        "country": json_data["city"]["country"],
+                        "city": json_data["city"]["name"],
+                        "time": datetime.utcfromtimestamp(dt).strftime("%H:%M"),
+                        "summary": json_data["list"][2]["weather"][0]["description"],
+                        "apparentTemperature": json_data["list"][2]["main"]["feels_like"],
+                        "temperature": json_data["list"][2]["main"]["temp"],
+                        "wind": json_data["list"][2]["wind"]["speed"],
+                        "humidity": json_data["list"][2]["main"]["humidity"] / 100,
+                        "icon": json_data["list"][2]["weather"][0]["icon"],
+                        "+2": json_data["list"][4]["main"]["temp"],
+                        "+4": json_data["list"][6]["main"]["temp"],
+                        "+6": json_data["list"][8]["main"]["temp"],
+                        "+8": json_data["list"][10]["main"]["temp"],
+                        "+10": json_data["list"][12]["main"]["temp"],
+                        "+12": json_data["list"][14]["main"]["temp"],
+                    }
+                
+            else:
+                response = requests.get(f"{self.weather_url}lat={lat}&lon={lng}&appid={self.weather_token}&lang={language}&units=metric")
+                if response.status_code == 200:
+                    json_data = response.json()
+                    timezone = json_data["city"]["timezone"]
+                    timestamp = round(timestamp / 3600) * 3600 + timezone # getting closest existing timestamp to the chosen timestamp
+                    for weather in range(96): # owm always gives 96 timestamps, no need to count them from json
+                        if str(json_data["list"][weather]["dt"]) == str(timestamp):
+                            return {
+                                "country": json_data["city"]["country"],
+                                "city": json_data["city"]["name"],
+                                "time": datetime.utcfromtimestamp(timestamp).strftime("%H:%M"),
+                                "summary": json_data["list"][weather]["weather"][0]["description"],
+                                "apparentTemperature": json_data["list"][weather]["main"]["feels_like"],
+                                "temperature": json_data["list"][weather]["main"]["temp"],
+                                "wind": json_data["list"][weather]["wind"]["speed"],
+                                "humidity": json_data["list"][weather]["main"]["humidity"] / 100,
+                                "icon": json_data["list"][weather]["weather"][0]["icon"],
+                                "+2": json_data["list"][weather + 2]["main"]["temp"],
+                                "+4": json_data["list"][weather + 4]["main"]["temp"],
+                                "+6": json_data["list"][weather + 6]["main"]["temp"],
+                                "+8": json_data["list"][weather + 8]["main"]["temp"],
+                                "+10": json_data["list"][weather + 10]["main"]["temp"],
+                                "+12": json_data["list"][weather + 12]["main"]["temp"],
+                            }
+
+
+
+
+
+
+            
--- weatherapi.py
+++ weatherapi.py
@@ -2,30 +2,32 @@ from starlette.responses import StreamingResponse
 import config
 from utils import WeatherAPI, Render
 from fastapi import FastAPI
+from typing import Optional
+from loguru import logger
 
 import utils
 
 app = FastAPI()
 
+weather = WeatherAPI(config.OWM_TOKEN)
 
-@app.get(
-    "/{language}/{city}",
-    responses={
-        200: {
-            "content": {
-                "application/json": {  # Should we make 4xx?
-                    "example": {"status": "error", "message": "location not found"},
-                },
-                "image/png": {},
-            },
-        },
-    },
-)
-def read_root(language: utils.LANGUAGES, city: str):
-    weather = WeatherAPI(config.OWM_TOKEN)
-    info = weather.get_weather(city, language=language.name)
-    if info == None:
-        return {"status": "error", "message": "location not found"}
-    else:
-        image = Render().make_hourly(info, language.name)
-        return StreamingResponse(image, media_type="image/jpeg")
+@app.get("/{language}/{city}")
+async def with_timestamp(language: utils.LANGUAGES, city: str, timestamp: Optional[int] = None):
+    if timestamp != None:
+        info = weather.get_weather(city, language=language.name, timestamp=timestamp)
+        if info == None:
+            return {"status": "error", "message": "location not found"}
+        else:
+            image = Render().make_hourly(info, language.name)
+            return StreamingResponse(image, media_type="image/jpeg")
+    else:        
+        info = weather.get_weather(city, language=language.name)
+        if info == None:
+            return {"status": "error", "message": "location not found"}
+        else:
+            image = Render().make_hourly(info, language.name)
+            return StreamingResponse(image, media_type="image/jpeg")
+       
+        
+
+
